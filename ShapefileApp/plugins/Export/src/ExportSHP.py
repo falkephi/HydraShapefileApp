045ad267a371f15dd7362bf3dc244d73f8b70a40
Philipp Meier <philipp.meier@eawag.ch>
2016-11-16 16:08:42 +0100

    Moved lib

--- ShapefileApp/plugins/Export/src/ExportSHP.py
+++ ShapefileApp/plugins/Export/src/ExportSHP.py
@@ -21,7 +21,7 @@
 import os
 import sys
 
-sys.path.append(os.path.sep.join(['..', '..', '..', '..', 'lib']))
+sys.path.append(os.path.sep.join(['..', '..', '..', 'lib']))
 
 from hydra_network import HydraNetworkTree
 from shapefile_lib import ShapefileApp
